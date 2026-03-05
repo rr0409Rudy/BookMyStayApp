bf069be38bd320768c9b0e70917e49d796837cc6
SriNeha06 <srinehavanjavaka@gmail.com>
2026-03-05 15:17:43 +0530

    UseCase2

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,5 +1,65 @@
+// File: UseCase2RoomInitialization.java
+
 public class BookMyStayApp {
-    public static void main (String[] args){
-        System.out.println("Hotel Booking system v1.0.000");
+    public static void main(String[] args) {
+        System.out.println("Hotel Booking System v2.1.000");
+
+        // Availability variables (static representation)
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
+
+        // Initialize room objects
+        Room single = new SingleRoom(1, 200, 1000.0);
+        Room doubleR = new DoubleRoom(2, 350, 1800.0);
+        Room suite = new SuiteRoom(3, 600, 3500.0);
+
+        // Display details
+        single.displayRoomDetails(singleRoomAvailability);
+        doubleR.displayRoomDetails(doubleRoomAvailability);
+        suite.displayRoomDetails(suiteRoomAvailability);
+
+        System.out.println("Application terminated.");
+    }
+}
+
+// Abstract class
+abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails(int availability) {
+        System.out.println(getClass().getSimpleName() + " Details:");
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sq.ft");
+        System.out.println("Price per Night: ₹" + pricePerNight);
+        System.out.println("Available Rooms: " + availability);
+        System.out.println("-----------------------------");
+    }
+}
+
+// Concrete classes
+class SingleRoom extends Room {
+    public SingleRoom(int beds, int size, double price) {
+        super(beds, size, price);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom(int beds, int size, double price) {
+        super(beds, size, price);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom(int beds, int size, double price) {
+        super(beds, size, price);
     }
 }
